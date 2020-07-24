6b97833131e3550621ef373f35de923cf39a8115
jeantimex <jean.timex@gmail.com>
2020-07-24 15:13:47 -0700

    Allow me to add multiple pages.

--- src/linear-regression.js
+++ src/linear-regression.js
@@ -1,3 +1,7 @@
+/**
+ * Tensorflow Linear Regression Demo
+ * https://www.youtube.com/watch?v=dLp10CFIvxI&list=PLRqwX-V7Uu6YIeVA3dNxbR9PYj4wV31oQ&index=5
+ */
 import * as tf from '@tensorflow/tfjs';
 import p5 from 'p5';
 
