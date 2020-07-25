e65a53aa136d0f0063985dfc0a2ec58da5cf0257
jeantimex <jean.timex@gmail.com>
2020-07-24 18:00:24 -0700

    Add visualize demo.

---
+++ src/visualize-demo.js
@@ -0,0 +1,24 @@
+import * as tfvis from '@tensorflow/tfjs-vis';
+
+const apples = Array(14)
+  .fill(0)
+  .map(y => Math.random() * 100 + Math.random() * 50)
+  .map((y, x) => ({ x: x, y: y }));
+
+const oranges = Array(14)
+  .fill(0)
+  .map(y => Math.random() * 100 + Math.random() * 150)
+  .map((y, x) => ({ x, y }));
+
+const series = ['Apples', 'Oranges'];
+
+const data = { values: [apples, oranges], series };
+
+const container = document.getElementById('scatter-cont');
+tfvis.render.scatterplot(container, data, {
+  xLabel: 'day',
+  yLabel: 'sales',
+  height: 450,
+  zoomToFit: true,
+  fontSize: 16,
+});
